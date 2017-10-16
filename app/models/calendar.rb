1692072a4116d8fe88ccb380e1359b5ac8c31f14
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2017-10-16 12:27:47 +0900

    fix

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -3,7 +3,5 @@ class Calendar < ApplicationRecord
 
   enum sex_type: { unknown: 0, contraception: 1, to_go_out: 2, to_go_in: 3 }
   enum feel_type: { fine: 0, like: 1, soso: 2, bad: 3 }
-
-
-
+  
 end
